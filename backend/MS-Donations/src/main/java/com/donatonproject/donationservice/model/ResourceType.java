e90d57d58782ef3990e59a5b58ca84a630fd7833
SmCarrera22 <se.carrera@duocuc.cl>
2026-06-16 20:08:02 -0400

    Creating Auth Service

--- backend/MS-Donations/src/main/java/com/donatonproject/donationservice/model/ResourceType.java
+++
@@ -1,7 +0,0 @@
-package com.donatonproject.donationservice.model;
-
-public enum ResourceType {
-    ALIMENTOS,
-    ROPA,
-    INSUMOS_MEDICOS
-}
--- backend/MS-Donations/src/main/java/com/donatonproject/donationservice/repository/DonationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.donatonproject.donationservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.donatonproject.donationservice.model.Donation;
-
-public interface DonationRepository extends JpaRepository<Donation, Long> {
-
-}
